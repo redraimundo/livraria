a14df416bb138567b57ae81267d18633c5d651af
Karina Bianek <bianekkarina@gmail.com>
2024-10-02 01:21:09 -0300

    mudei tb

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
+const userRoutes = require('./routes/usersRoutes')
 const livrosRoute = require('./routes/livrosRoutes')
 
 dotenv.config()
@@ -10,6 +11,7 @@ const app = express()
 app.use(express.json())
 
 app.use('/api', userRoutes)
+app.use('/api', livrosRoute)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
